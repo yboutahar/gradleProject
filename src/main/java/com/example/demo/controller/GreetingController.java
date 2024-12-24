665689760f1c841089b8978b40779589c33b78e0
Yassine BOUTAHAR <yassineboutahar@gmail.com>
2024-12-24 12:43:54 +0100

    comment

--- src/main/java/com/example/demo/controller/GreetingController.java
+++ src/main/java/com/example/demo/controller/GreetingController.java
@@ -13,6 +13,11 @@ public class GreetingController {
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
+	/**
+	 *
+	 * @param name
+	 * @return
+	 */
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
