63815b78dfa2bca0af0a4235fe531b73a2f41b7d
Yassine BOUTAHAR <yassineboutahar@gmail.com>
2024-12-26 12:25:24 +0100

    add message controller

--- src/main/java/com/example/demo/controller/GreetingController.java
+++ src/main/java/com/example/demo/controller/GreetingController.java
@@ -17,4 +17,9 @@ public class GreetingController {
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
+
+	@GetMapping("/message")
+	public String message(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return "Hello word";
+	}
 }
